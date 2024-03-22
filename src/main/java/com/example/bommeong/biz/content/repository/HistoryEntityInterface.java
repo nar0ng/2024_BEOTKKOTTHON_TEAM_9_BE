9e80650950d2eda522a23079aad578692c08d9e4
kwon416 <bbbggg111@naver.com>
2024-03-22 14:08:05 +0900

    Remove: 미사용 패키지 제거

--- src/main/java/com/example/bommeong/biz/content/repository/HistoryEntityInterface.java
+++
@@ -1,6 +0,0 @@
-package com.example.bommeong.biz.content.repository;
-
-public interface HistoryEntityInterface {
-    Long getContentId();
-    String getTitle();
-}
--- src/main/java/com/example/bommeong/biz/content/repository/TagRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bommeong.biz.content.repository;
-
-import com.example.bommeong.biz.content.dto.TagEntity;
-import com.example.bommeong.common.dao.BaseRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TagRepository extends BaseRepository<TagEntity, Long> {
-}
