d62b4de8f8f32ce7d968d57b357a62184843a365
nar0ng <71819594+nar0ng@users.noreply.github.com>
2024-07-27 00:42:03 +0900

    fix: 입양 신청서 항목의 request 수정

--- src/main/java/com/example/bommeong/biz/adopt/dto/AdoptDtoReq.java
+++ src/main/java/com/example/bommeong/biz/adopt/dto/AdoptDtoReq.java
@@ -8,43 +8,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
-@Data
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class AdoptDtoReq {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @JsonIgnore
-    private Long id;
+public record AdoptDtoReq (
+        Long id,
+        MultipartFile uploadFile,
+        String petHistory,
+        String reasonForAdoption,
+        String currentPet
 
-    @Column(name = "first_consent")
-    private String firstConsent;
+){
 
-    @Transient
-    private MultipartFile uploadFile;
-
-    @Column(name = "first_response")
-    private String firstResponse;
-
-    @Column(name="second_response")
-    private String secondResponse;
-
-    @Column(name="third_response")
-    private String thirdResponse;
-
-    @Column(name="fourth_response")
-    private String fourthResponse;
-
-    @Column(name="first_adoption_response")
-    private String firstAdoptionResponse;
-
-    @Column(name="second_adoption_response")
-    private String secondAdoptionResponse;
-
-    @Column(name="third_adoption_response")
-    private String thirdAdoptionResponse;
-
-    @Column(name="fourth_adoption_response")
-    private String fourthAdoptionResponse;
 }
